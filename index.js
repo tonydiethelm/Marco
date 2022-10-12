081eb6f089746432746021bf9ea49e76f813e5bd
Tony Diethelm <tonydiethelm@gmail.com>
2022-10-12 16:26:32 -0700

    Initial commit. two buttons, both console log need to enable fetches

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+What do I need this to do? 
+put event handlers and functions on my buttons in index.HTML to...
+send a fetch request to localhost/marco. That's it. 
+
+Destructive Marco will send looped fetch requests. It's meant to DDOS the Polo API backend. 
+*/
+
+
+// Setup stuff.
+console.log("JS is loading")
+
+const marcoButton = document.getElementById('marco');
+const destructiveMarcoButton = document.getElementById('destructiveMarco');
+
+marcoButton.addEventListener("click", sayMarco);
+destructiveMarcoButton.addEventListener("click", yellMarco);
+
+
+function sayMarco(){
+  console.log('Marco!')
+};
+
+function yellMarco(){
+  console.log('MARCO!')
+};
+
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+/*Since webpack bundles everything and creates a simple browser-readable code, 
+all packages, presets and plugins you will install need to be configured in webpack.
+https://webpack.js.org/configuration/
+*/
+
+//HTMLWebPackPlugin stuff. 
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./index.html",   //file it will use as a template to build...
+  //filename: "./HWPP-index.html"           //this file here.   Commented out, works
+});
+
+module.exports = {
+  //various configurations for webpack. 
+  entry: "./index.js",        //application starts running and webpack starts bundling here. 
+  output: {
+    path: path.join(__dirname, 'Public'), //target directory for all output files. 
+    filename: "bundle.js",                 //Puts files by name in dir specified above.
+    publicPath: '/',                      //Where you uploaded your bundled files. Relative to filename above?
+  },
+  //mode: 'development',                    //Shouldn't need this, I'm calling "--mode development" in the package.json dev script.
+  devServer: {                            //Need to configure the dev server to serve up front end stuff. 
+    host: 'localhost',
+    port: 8080,
+    //match the output path.              Why?
+    static: {
+      directory: path.join(__dirname, 'Public'),
+      publicPath: '/',
+    },
+    //hot: true,                                     //I'm calling this in the package.json script. Not needed here. 
+    headers: {'Tony Diethelm': 'Was Here. :D'},      //This is added to all headers. Fun! 
+    proxy: {
+      //I need to set up the proxy server for the front end stuff to 
+      //make proper get/post requests to the back end. 
+      '/marco': {target: 'http://localhost:3000/', secure: false,},
+    }
+
+  },
+  plugins: [htmlPlugin],                //rules for HTML Web Pack Plugin. Defined above. 
+  module: {                             //configuration for the various modules. 
+    rules:[
+      {
+        test: /.(css|scss)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  }
+};
